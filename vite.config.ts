527ea6ba0a0fcd339184933c050d3b14630367c0
Ahmed Ismail <ahismail2005@gmail.com>
2025-12-19 18:19:46 +0000

    resoulve conflit

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -6,38 +5,22 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
 
-=======
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 99a7da8f2121b9440cc0997a2fd1d569cd689e6a
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-<<<<<<< HEAD
 
-=======
->>>>>>> 99a7da8f2121b9440cc0997a2fd1d569cd689e6a
   build: {
     target: 'esnext',
     outDir: 'dist',
   },
-<<<<<<< HEAD
 
-=======
->>>>>>> 99a7da8f2121b9440cc0997a2fd1d569cd689e6a
   server: {
     port: 3000,
     open: true,
   },
-<<<<<<< HEAD
 
   // 👇 أضف هذا الجزء فقط
   test: {
@@ -46,6 +29,5 @@ export default defineConfig({
     setupFiles: './src/test/setup.ts',
   },
 })
-=======
-});
->>>>>>> 99a7da8f2121b9440cc0997a2fd1d569cd689e6a
+
+
